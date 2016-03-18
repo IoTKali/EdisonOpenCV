e8ba96344890d2281b1544c02a2fbe332065f616
Marcko45 <marcko.rp@gmail.com>
2016-03-18 02:44:52 -0600

    implement button servo and plates

--- motorx.py
+++ motorx.py
@@ -1,24 +1,26 @@
 import time
 import pyupm_servo as servo 
 
-# Create the servo object using D5
-gServo = servo.ES08A(5)
 
-for i in range(0,10): 
-    # Set the servo arm to 0 degrees
-    gServo.setAngle(0)
-    print 'Set angle to 0'
-    time.sleep(1)
+def servo():
+    # Create the servo object using D5
+    gServo = servo.ES08A(5)
 
-    # Set the servo arm to 90 degrees
-    gServo.setAngle(90)
-    print 'Set angle to 90'
-    time.sleep(1)
+    for i in range(0,10): 
+        # Set the servo arm to 0 degrees
+        gServo.setAngle(0)
+        print 'Set angle to 0'
+        time.sleep(1)
 
-    # Set the servo arm to 180 degrees
-    #gServo.setAngle(180)
-    #print 'Set angle to 180'
-    #time.sleep(1)
+        # Set the servo arm to 90 degrees
+        gServo.setAngle(90)
+        print 'Set angle to 90'
+        time.sleep(1)
 
-# Delete the servo object
-del gServo 
+        # Set the servo arm to 180 degrees
+        #gServo.setAngle(180)
+        #print 'Set angle to 180'
+        #time.sleep(1)
+
+    # Delete the servo object
+    del gServo 
